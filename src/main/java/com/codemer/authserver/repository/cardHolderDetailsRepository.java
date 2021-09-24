7a80bb9743b92c1b167baa5ed1289ef656155f41
Naga Phanindra Ravuri <51380245+NagaPhanindra-Repository@users.noreply.github.com>
2021-09-24 16:45:04 -0500

    Update cardHolderDetailsRepository.java

--- src/main/java/com/codemer/authserver/repository/cardHolderDetailsRepository.java
+++ src/main/java/com/codemer/authserver/repository/cardHolderDetailsRepository.java
@@ -1,5 +1,11 @@
 package com.codemer.authserver.repository;
 
-public interface cardHolderDetailsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mastercard.cardholder.model.cardHolderDetails;
+
+public interface cardHolderDetailsRepository extends JpaRepository<cardHolderDetails, Integer> {
+
+	cardHolderDetails findByUserName(String userName);
 
 }
