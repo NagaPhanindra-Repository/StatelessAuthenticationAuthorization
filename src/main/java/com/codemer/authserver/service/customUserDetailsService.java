fbd8b7b3e1fa85a4015aea1cd2c3106043be4329
Naga Phanindra Ravuri <51380245+NagaPhanindra-Repository@users.noreply.github.com>
2021-09-24 16:47:06 -0500

    Update customUserDetailsService.java

--- src/main/java/com/codemer/authserver/service/customUserDetailsService.java
+++ src/main/java/com/codemer/authserver/service/customUserDetailsService.java
@@ -1,5 +1,30 @@
 package com.codemer.authserver.service;
 
-public class customUserDetailsService {
+
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mastercard.cardholder.model.cardHolder;
+import com.mastercard.cardholder.repository.cardHolderRepository;
+
+@Service
+public class customUserDetailsService implements UserDetailsService{
+
+	 @Autowired
+	 private cardHolderRepository cardHolderRepo;
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+	cardHolder cardholder=	cardHolderRepo.findUserByUserName(username);
+		
+		return new User(cardholder.getUserName(),cardholder.getPassword(),new ArrayList());
+	}
 
 }
