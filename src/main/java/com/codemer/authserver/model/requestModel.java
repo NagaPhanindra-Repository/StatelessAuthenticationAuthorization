78f7d3c1597748ea58b1567f0a8f3a673d3d13c6
Naga Phanindra Ravuri <51380245+NagaPhanindra-Repository@users.noreply.github.com>
2021-09-24 16:43:55 -0500

    Update requestModel.java

--- src/main/java/com/codemer/authserver/model/requestModel.java
+++ src/main/java/com/codemer/authserver/model/requestModel.java
@@ -1,5 +1,28 @@
 package com.codemer.authserver.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class requestModel {
+	
+	private String username;
+	private String password;
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
 
 }
