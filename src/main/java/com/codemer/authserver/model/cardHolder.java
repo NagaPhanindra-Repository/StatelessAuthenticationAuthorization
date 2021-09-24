55ceed7bab8b7b4061a2648301e4e55ccfe8564a
Naga Phanindra Ravuri <51380245+NagaPhanindra-Repository@users.noreply.github.com>
2021-09-24 16:40:57 -0500

    Update cardHolder.java

--- src/main/java/com/codemer/authserver/model/cardHolder.java
+++ src/main/java/com/codemer/authserver/model/cardHolder.java
@@ -1,5 +1,65 @@
 package com.codemer.authserver.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "CARDHOLDER_TBL")
 public class cardHolder {
+	
+	@Id
+	private int id;
+	private String userName;
+	private String password;
+	private String email;
+	public cardHolder(int id, String userName, String password, String email) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+	}
+	public cardHolder() {
+		super();
+		
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "cardHolder [id=" + id + ", userName=" + userName + ", password=" + password + ", email=" + email + "]";
+	}
+	
+	
 
 }
